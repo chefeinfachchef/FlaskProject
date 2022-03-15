319ee6afac3869fe85ea13d4a5bfdcf1f21e5b2d
laurenz fischbacher <laurenz.fischbacher@bulme.at>
2022-03-15 10:03:52 +0100

    überarbeitet

--- controllers/blueprint.py
+++ controllers/blueprint.py
@@ -1,5 +1,5 @@
 from ast import Add
-from flask import Flask, request
+from flask import Flask, redirect, request
 from flask.templating import render_template
 from flask import Blueprint
 import sqlalchemy
@@ -17,22 +17,37 @@ def Reise():
     #alle products laden
     Reise = session.query(Reise).order_by(Reise.ReiseId).all()
 
-    return render_template("products.html", products = Reise)
+    return render_template("products.html", Reise = Reise)
 
 
 @Reise_blueprint.route("/products/add", methods=["GET","POST"])
 def products_add():
     session : sqlalchemy.orm.scoping.scoped_session = db.session
-    productlines = session.query(Reiseteilnehmer).order_by(Reiseteilnehmer.ReisendeId).all()
+    productlines = session.query(Reise).order_by(Reise).all()
     
     addProductForm = AddItemForm()
 
     if request.method == 'POST':
         
         if addProductForm.validate_on_submit():
-            print("gültig")
-            return render_template("products_add.html",productlines=productlines, form = addProductForm)
+            #hier in db einfügen
+            newReise = Reise()
+
+            newReise.productCode = addProductForm.productCode.data
+            newReise.productName = addProductForm.productName.data
+            newReise.productLine = addProductForm.productLine.data
+            newReise.productScale = addProductForm.productScale.data
+            newReise.productVendor = addProductForm.productVendor.data
+            newReise.productDescription = addProductForm.productDescription.data
+            newReise.quantityInStock = addProductForm.quantityInStock.data
+            newReise.buyPrice = addProductForm.buyPrice.data
+            newReise.MSRP = addProductForm.MSRP.data
+
+            db.session.add(newReise)
+            db.session.commit()
+
+            return redirect("/products")
         else:
-            raise "Fatal"
+            return render_template("products_add.html",productlines=productlines,form = addProductForm)
     else:
         return render_template("products_add.html",productlines=productlines,form = addProductForm)
